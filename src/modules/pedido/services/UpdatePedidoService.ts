2768a206569a25a923aa313f9f0601272cf9ada2
Diogo Diarone Alves de Almeida <da50084@saqueepague.com.br>
2023-02-26 23:00:37 -0300

    bugfix: ajuste no parametros para updadte situação pedido

--- src/modules/pedido/services/UpdatePedidoService.ts
+++ src/modules/pedido/services/UpdatePedidoService.ts
@@ -2,10 +2,10 @@ import AppError from "@shared/erros/AppError";
 import PedidoRepository from "../repositories/PedidoRepository";
 
 class UpdatePedidoService {
-  public async execute(tenantId: string, filtro: object, update: object, ): Promise<number> {
+  public async execute(tenantId: string, idPedidos: number[], filtro: object, ): Promise<number> {
     const pedidoRepository = new PedidoRepository()
 
-    const pedido = pedidoRepository.update({tenantId, ...filtro}, {update})
+    const pedido = pedidoRepository.update(tenantId, idPedidos, filtro)
 
     if(!pedido) new AppError(`Não foi possível atualizar o(s) pedido(s) ${filtro}`)
 
