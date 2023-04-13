67deddec9cbcf41b85f35bf9571f68839bf38e72
Diogo Diarone Alves de Almeida <da50084@saqueepague.com.br>
2023-04-13 14:22:11 -0300

    feature: versão sistema e inclusão coluna subclasse tabela produto

---
+++ migrations/20230413171333_AlteraTabelaProduto.ts
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('produto', tabela => {
+    tabela.string('subclasse', 32)
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('produto', table => {
+    table.dropColumn('subclasse')
+  })
+}
