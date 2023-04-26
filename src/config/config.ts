f436c1f4a7dc869bfb108f442257b587c26e67ec
Diogo Diarone Alves de Almeida <da50084@saqueepague.com.br>
2023-04-26 16:20:58 -0300

    improvement: versão

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import { portaApi } from '@shared/consts/ambiente'
 import 'dotenv/config'
 
 export default {
-  versao: 20230420,
+  versao: 20230426,
   cors: {
     //TODO revisar politica de CORS
     methods: 'GET, OPTIONS, PUT, POST, DELETE',
