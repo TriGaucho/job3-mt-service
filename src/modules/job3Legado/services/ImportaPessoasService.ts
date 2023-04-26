444a8dd1613515820b562c37557a400d380ab8e1
Diogo Diarone Alves de Almeida <da50084@saqueepague.com.br>
2023-04-26 16:18:14 -0300

    improvement: implementado transaction para execução do updade

--- src/modules/job3Legado/services/ImportaPessoasService.ts
+++ src/modules/job3Legado/services/ImportaPessoasService.ts
@@ -3,6 +3,7 @@ import InsereTenantId from '@shared/utils/insereTenantId'
 import LegadoJob3Repository from '../repositories/LegadoJob3Repository'
 
 import Pessoa from '@modules/pessoa/entities/Pessoa'
+import { pessoa } from '@shared/consts/banco'
 
 class ImportaPessoasService {
   public async execute(pessoasArray:Pessoa[], tenantId: string): Promise<number> {
@@ -10,7 +11,7 @@ class ImportaPessoasService {
 
     const legadoJob3Repository = new LegadoJob3Repository()
 
-    const pessoas = legadoJob3Repository.createOrUpdate(pessoasArray, 'pessoa')
+    const pessoas = legadoJob3Repository.inativaCriaOuAtualiza(pessoasArray, pessoa, tenantId)
 
     if (!pessoas) throw new AppError('Não foi possível importar as pessoas.')
 
--- src/modules/job3Legado/services/ImportaProdutosService.ts
+++ src/modules/job3Legado/services/ImportaProdutosService.ts
@@ -3,6 +3,7 @@ import InsereTenantId from '@shared/utils/insereTenantId'
 import LegadoJob3Repository from '../repositories/LegadoJob3Repository'
 
 import Produto from '@modules/produto/entities/Produto'
+import { produto } from '@shared/consts/banco'
 
 class ImportaProdutosService {
   public async execute(produtosArray:Produto[], tenantId: string): Promise<number> {
@@ -10,7 +11,7 @@ class ImportaProdutosService {
 
     const legadoJob3Repository = new LegadoJob3Repository()
 
-    const produtos = legadoJob3Repository.createOrUpdate(produtosArray, 'produto')
+    const produtos = legadoJob3Repository.inativaCriaOuAtualiza(produtosArray, produto, tenantId)
 
     if (!produtos) throw new AppError('Não foi possível importar os produtos.')
 
