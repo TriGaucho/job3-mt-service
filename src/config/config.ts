1bda92e3f227d9be5cb6fe0096d70b5797e9ba7d
Diogo Almeida <diogo254@gmail.com>
2023-09-11 17:23:06 -0300

    versão 20230911

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import { portaApi } from '@shared/consts/ambiente'
 import 'dotenv/config'
 
 export default {
-  versao: 20230605,
+  versao: 20230911,
   cors: {
     //TODO revisar politica de CORS
     methods: 'GET, OPTIONS, PUT, POST, DELETE',
