376c0a3599db4ee8f83108dbdd59501a0ebc3292
Diogo <diogo.almeida@verbin.com.br>
2025-11-07 22:36:08 -0300

    versao

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import { portaApi } from '@shared/consts/ambiente'
 import 'dotenv/config'
 
 export default {
-  versao: 20240720,
+  versao: 20251107,
   cors: {
     //TODO revisar politica de CORS
     methods: 'GET, OPTIONS, PUT, POST, DELETE',
